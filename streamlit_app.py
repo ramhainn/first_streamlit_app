d88a627c700b26ba36a43f12bd7c5d988d32e99e
ramhainn <90064742+ramhainn@users.noreply.github.com>
2023-03-17 16:46:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ try:
     else:
         back_from_function=get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-          
+except:
+    pass
 streamlit.write('The user entered ', fruit_choice)
 
 #don't run anything past here while we troubleshoot
