227d746062ef33c1b392c2e542181d86cb594029
ramhainn <90064742+ramhainn@users.noreply.github.com>
2023-03-17 16:31:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,12 +36,12 @@ def get_fruityvice_date(this_fruit_choice):
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-   else:
-        back_from_function=get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+       streamlit.error("Please select a fruit to get information.")
+  else:
+       back_from_function=get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
     
  #   
 streamlit.write('The user entered ', fruit_choice)
